1e537f83eb5a6dba0836a967523a954970fb4cea
zhvial <60042073+zhvial@users.noreply.github.com>
2026-04-02 09:18:27 +0300

    Update script URLs for Fieldwire download names

--- Fieldwire- Fix Download Names.js
+++ Fieldwire- Fix Download Names.js
@@ -8,8 +8,8 @@
 // @grant        GM_download
 // @grant        GM_xmlhttpRequest
 // @connect      files.us.fieldwire.com
-// @updateURL    https://raw.githubusercontent.com/zhvial/UserScripts/main/Fieldwire-%20Fix%20Download%20Names%20V9.1%20(.msg%20Fix).js
-// @downloadURL  https://raw.githubusercontent.com/zhvial/UserScripts/main/Fieldwire-%20Fix%20Download%20Names%20V9.1%20(.msg%20Fix).js
+// @updateURL    https://raw.githubusercontent.com/zhvial/UserScripts/main/Fieldwire-%20Fix%20Download%20Names.js
+// @downloadURL  https://raw.githubusercontent.com/zhvial/UserScripts/main/Fieldwire-%20Fix%20Download%20Names.js
 // ==/UserScript==
 
 (function() {
